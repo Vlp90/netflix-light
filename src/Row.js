9860c3807f1fda62262f4cf65886386adfe267fd
Vlp90 <vladimir.leloup@gmail.com>
2020-09-07 14:53:12 +0200

    add trailer event on img

--- src/Row.js
+++ src/Row.js
@@ -3,10 +3,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerURL] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -20,20 +23,51 @@ function Row({ title, fetchUrl, isLargeRow }) {
   // if [], run once and not again
   // if we use a variable outside de useEffect block, we have to fill the dependencie [fetchUrl]
 
-//   console.log(movies);
+  //   console.log(movies);
+
+  // YouTube SETTINGS
+  const opts = {
+    height: "390px",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    // if trailer URL is open then close it
+    if (trailerURL) {
+      setTrailerURL("");
+    } else {
+      movieTrailer(movie?.originale_name || movie?.title || movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=R36D1IWmf9A
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerURL(urlParams.get("v"));
+          console.log("URL PARAMS", urlParams);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className={`row__posters ${isLargeRow && 'row__postersLarge' }`}>
+      <div className={`row__posters ${isLargeRow && "row__postersLarge"}`}>
         {movies.map((movie) => (
           <img
+            onClick={() => handleClick(movie)}
             key={movie.id}
             className="row__poster"
-            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
       </div>
+      {/* if we have trailer youtube video then show it, if not doesnt show youtube player  */}
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
   );
 }
