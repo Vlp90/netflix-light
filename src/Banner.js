b218cc32e2e3777c44d159c55ad86d07faf4dc95
Vlp90 <vladimir.leloup@gmail.com>
2020-09-07 11:53:15 +0200

    banner done

--- src/Banner.js
+++ src/Banner.js
@@ -28,9 +28,38 @@ function Banner() {
 
   console.log(movie);
 
+  // ELLYPSIS FONCTION
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <div>
-      <header></header>
+      <header
+        className="banner"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+          backgroundPosition: "top center",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.originale_name}
+          </h1>
+
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My list</button>
+          </div>
+
+          <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
+        </div>
+
+        <div className="banner--fadebottom">
+          
+        </div>
+      </header>
     </div>
   );
 }
